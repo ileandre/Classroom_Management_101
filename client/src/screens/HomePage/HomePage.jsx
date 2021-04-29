d17b7c64ff691eb73e09fc323ab6d4a6a4d44ee9
irma leandre <irmaleandre@irmas-Air.PK5001Z>
2021-04-29 13:36:29 -0400

    chalk board created

--- client/src/screens/HomePage/HomePage.jsx
+++ client/src/screens/HomePage/HomePage.jsx
@@ -1,16 +1,19 @@
 import './HomePage.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function HomePage(props) {
     return (
         <div className="homepage">
-            <p>Teacher Quote(s)</p>
-            <div className='studentRoster'>
-                <Link to='/students'>
-                    <p>Student Roster</p>
-                </Link>
+            <div className='board-border'>
+                <div className='board'>
+                    <p>Teacher Quote(s)</p>
+                    <div className='studentRoster'>
+                        <Link to='/students'>
+                            <p>Student Roster</p>
+                        </Link>
+                    </div>
+                </div>
             </div>
-            <p></p>
         </div>
     )
 }
