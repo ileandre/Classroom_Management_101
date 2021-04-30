d32d80d119bac8e8e313f869ebaf1ad4d58d3a18
irma leandre <irmaleandre@irmas-Air.PK5001Z>
2021-04-30 13:47:17 -0400

    showstudent css completed

--- client/src/screens/ShowStudents/ShowStudents.jsx
+++ client/src/screens/ShowStudents/ShowStudents.jsx
@@ -1,12 +1,13 @@
 import './ShowStudents.css'
 import { useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import FilterButton from '../../components/FilterButton/FilterButton'
 // import PeriodButton from '../../components/PeriodButton/PeriodButton'
 
 function ShowStudents({ students, handleFilter, handlePeriod, queryStudents, fetchStudents, fetchAllComments }) {
     // console.log(students)
     // debugger
+const history = useHistory()
 
     const filterButtons = ['Top 5', 'A-Student', 'B-Student', 'C-Student', 'D-Student', 'F-Student']
     const periodButtons = ['Period 1', 'Period 2', 'Period 3', 'Period 4', 'Period 5', 'All']
@@ -21,36 +22,72 @@ function ShowStudents({ students, handleFilter, handlePeriod, queryStudents, fet
         handleRoster()
     }, [])
 
+
+    //BACK TO NORMAL!!!!!!!!!!!!!!!!
     return (
         <div className="showStudents">
             <div className="clipboard-border">
                 <div className="clipboard">
                     <h3 className="roster-title">Student Roster</h3>
-                    <div className='column-title student'>
-                        <p>First</p>
-                        <p>Last</p>
-                        <p>Grade</p>
-                        <p>Period</p>
-                    </div>
                     <div className="students-list">
-                        {queryStudents.map(student => (
-                            <Link to={`/students/${student.id}`} key={student.id} className='student'>
-                                <>
-                                    <p>{student.firstName}</p>
-                                    <p>{student.lastName}</p>
-                                    <p>{student.grade}</p>
-                                    <p>{student.period}</p>
-                                </>
-                            </Link>
-                        ))}
+                        <div className='column student'>
+                            <p className="column-title">First
+                                </p>
+                            <div className="students">
+                                {queryStudents.map(student => (
+                                    <Link to={`/students/${student.id}`} key={student.id} className='student'>
+                                        <>
+                                            <p>{student.firstName}</p>
+                                        </>
+                                    </Link>
+                                ))}
+                            </div>
+                        </div>
+                        <div className='column student'>
+                            <p className="column-title">Last</p>
+                            <div className="students">
+                                {queryStudents.map(student => (
+                                    <Link to={`/students/${student.id}`} key={student.id} className='student'>
+                                        <>
+                                            <p>{student.lastName}</p>
+                                        </>
+                                    </Link>
+                                ))}
+                            </div>
+                        </div>
+                        <div className='column student'>
+                            <p className="column-title">Grade</p>
+                            <div className="students">
+                                {queryStudents.map(student => (
+                                    <Link to={`/students/${student.id}`} key={student.id} className='student'>
+                                        <>
+                                            <p>{student.grade}</p>
+                                        </>
+                                    </Link>
+                                ))}
+                            </div>
+                        </div>
+                        <div className='column student'>
+                            <p className="column-title">Period</p>
+                            <div className="students">
+                                {queryStudents.map(student => (
+                                    <Link to={`/students/${student.id}`} key={student.id} className='student'>
+                                        <>
+                                            <p>{student.period}</p>
+                                        </>
+                                    </Link>
+                                ))}
+                            </div>
+                        </div>
                     </div>
-                    <Link to="students/form" className='addStudent-button'>Add Student</Link>
+                    <button className='addStudent-button' onClick={() => {history.push('/students/form')}}>Add Student</button>
+                    {/* <Link to="/students/form" className='addStudent-button'>Add Student</Link> */}
                 </div>
             </div>
             <div className='filterButtons-container buttons'>
                 {
                     filterButtons.map((filter, index) => (
-                        <button  className="button red-buttons" key={index} onClick={() => handleFilter(filter)}>{filter}</button>
+                        <button className="button red-buttons" key={index} onClick={() => handleFilter(filter)}>{filter}</button>
                     ))
                 }
             </div>
