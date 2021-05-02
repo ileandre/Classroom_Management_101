18eeb50b2d702f963219fbde3282fb896f394f07
irma leandre <irmaleandre@irmas-Air.PK5001Z>
2021-05-02 19:32:27 -0400

    homepage completed

--- client/src/screens/HomePage/HomePage.jsx
+++ client/src/screens/HomePage/HomePage.jsx
@@ -1,26 +1,33 @@
 import './HomePage.css'
-import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
-function HomePage({fetchStudents, fetchAllComments}) {
+function HomePage({ fetchStudents, fetchAllComments }) {
+const history = useHistory()
 
     const handleRoster = async () => {
         await fetchStudents()
         await fetchAllComments()
+        history.push('/students')
     }
     return (
-        <div className="homepage">
-            <div className='board-border'>
-                <div className='board'>
-                    <p className='quote-title' id='quote-title'>Teacher Quote</p>
+        <form className="homepage" onClick={() => {handleRoster()}}>
+            <div className="roster-book">
+                {/* <Link to='/students' onClick={() => {handleRoster()}}> */}
+                    <p className="roster">Student Roster</p>
+                {/* </Link> */}
+            </div>
+            {/* <div className='board-border'> */}
+            {/* <div className='board'> } */}
+            {/* <p className='quote-title' id='quote-title'>Student Roster</p>
                     <p className='quote' id="quote">Do not let others define your success.</p>
                     <div className='studentRoster'>
                         <Link to='/students' onClick={()=>{handleRoster()}}>
                             <p className="roster">Student Roster</p>
                         </Link>
                     </div>
-                </div>
-            </div>
-        </div>
+                {/* </div> */}
+            {/* </div> */}
+        </form>
     )
 }
 
